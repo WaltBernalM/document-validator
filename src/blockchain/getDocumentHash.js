fecd3e5e015b916618962396416c7c7e87e98f7a
Walter Bernal <wltrbm@gmail.com>
2023-12-26 19:46:34 -0700

    Creation of function endpoint to retrieve hash doc

---
+++ src/blockchain/getDocumentHash.js
@@ -0,0 +1,33 @@
+const ethers = require("ethers")
+const getABI = require("./getABI")
+const { node, privateKey, contractAddress } = require("./constants")
+const config = require("dotenv").config
+config()
+
+
+const nodeUrl = node
+const provider = ethers.getDefaultProvider(nodeUrl)
+
+const wallet = new ethers.Wallet(privateKey, provider)
+const contractABI = getABI("src/blockchain/BookHashStorage.sol")
+const contract = new ethers.Contract(contractAddress, contractABI, wallet)
+
+async function getDocumentHash(transactionCode) {
+  try {
+    const documentHash = await contract.getBookHash(transactionCode)
+    console.log("Retrieved Document Hash:", documentHash)
+    return documentHash
+  } catch (error) {
+    if (error && error.info && error.info.error) {
+      // console.error("Smart contract call failed:", error.info.error.message)
+      return `Smart contract call failed:, ${error.info.error.message}`
+    } else {
+      // console.error("An unexpected error occurred:", error)
+      return `An unexpected error occurred: ${error}`
+    }
+  }
+}
+
+// getDocumentHash("2023-12-27T01:46:14.811Z-b5896c85-d31e-4c9c-ac3d-759e7ebff63b")
+
+module.exports = getDocumentHash
---
+++ src/functions/bkch-doc-entry.js
@@ -0,0 +1,61 @@
+const { app } = require('@azure/functions');
+const validTransactionId = require('../utils/validTransactionId');
+const getDocumentHash = require('../blockchain/getDocumentHash');
+
+app.http("bkch-doc-entry", {
+  methods: ["GET"],
+  authLevel: "anonymous",
+  handler: async (request, context) => {
+    context.log(`Http function processed request for url "${request.url}"`)
+    try {
+      const url = new URL(request.url)
+      const transactionId = url.searchParams.get("transactionId")
+
+      if (!transactionId) {
+        const response = {
+          message: "TransactionId parameter is missing in the query.",
+        }
+        return {
+          status: 400,
+          body: JSON.stringify(response),
+          headers: { "Content-Type": "application/json" },
+        }
+      }
+
+      if (!validTransactionId(transactionId)) {
+        const response = {
+          message: "TransactionId parameter is not a valid transaction."
+        }
+        return {
+          status: 400, 
+          body: JSON.stringify(response),
+          headers: { "Content-Type": "application/json" },
+        }
+      }
+
+      /****************************************************************************************************************/
+      /* Blockchain Interaction */
+      /****************************************************************************************************************/
+      const documentHash = await getDocumentHash(transactionId)
+
+      const response = {
+        transactionId: transactionId,
+        documentHash: documentHash
+      }
+
+      return {
+        status: 200,
+        body: JSON.stringify(response),
+        headers: { "Content-Type": "application/json" },
+      }
+    } catch (error) {
+      context.log.error(`Error processing request: ${error}`)
+      return {
+        status: 500, // Internal Server Error
+        body: "Internal Server Error",
+      }
+    }
+  }
+})
+
+
